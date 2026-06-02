b878caee49acf7ce0361990583c1896de5dc65ef
shen fangqi <shenfq@weic.jp>
2026-06-02 23:42:48 +0900

    Redeploy GitHub Pages assets

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"59aa584fdf100e6c78c785d8a5b565d1de4b48
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3615012962" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2798506646" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
